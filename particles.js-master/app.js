58bee8b6d77ccd783a9839eb7611ad9ec90c120b
Ben Yang <ben_yang@foxmail.com>
2018-02-12 22:25:54 -0500

    fix menu scroll

--- particles.js-master/app.js
+++ particles.js-master/app.js
@@ -55,7 +55,7 @@ particlesJS('particles-js',
     },
     "move": {
       "enable": true,
-      "speed": 0.45,
+      "speed": 0.5,
       "direction": "top",
       "random": false,
       "straight": false,
